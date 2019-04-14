9004517704dd9b50734d9f127edc3b545514f8a4
Sani <khursani8@gmail.com>
2019-04-14 11:15:03 +0900

    setup model

--- BlueprintArchitech/BijakSana/__init__.py
+++ BlueprintArchitech/BijakSana/__init__.py
@@ -3,37 +3,46 @@ from BlueprintArchitech.BijakSana.codeathonai.v1 import learn, predict, learn_an
 import numpy as np
 import json
 
-blueprint = Blueprint('bijaksana', __name__,template_folder='BlueprintArchitech/BijakSana')
-@blueprint.route('/learn', methods=['GET','POST'])
+blueprint = Blueprint('bijaksana', __name__,
+                      template_folder='BlueprintArchitech/BijakSana')
+
+
+@blueprint.route('/learn', methods=['GET', 'POST'])
 def learn_it():
-	if request.method == 'POST':
-		respond = {'status' : 200,'msj' : 'POST CALLED'}
-		data_target = np.array([float(x) for x in request.form['data_target'].split(',')]).reshape(3,2)
-		result = learn(request.form['userid'], data_target) 
-		return jsonify(result.modelid)
-	else:
-		respond = {'status' : 200,'msj' : 'GET CALLED'}
-		return jsonify(respond)
+    if request.method == 'POST':
+        respond = {'status': 200, 'msj': 'POST CALLED'}
+        data_target = np.array(
+            [float(x) for x in request.form['data_target'].split(',')]).reshape(3, 2)
+        result = learn(request.form['userid'], data_target)
+        return jsonify(result.modelid)
+    else:
+        respond = {'status': 200, 'msj': 'GET CALLED'}
+        return jsonify(respond)
 
-@blueprint.route('/predict', methods=['GET','POST'])
+
+@blueprint.route('/predict', methods=['GET', 'POST'])
 def predict_it():
-	if request.method == 'POST':
-		respond = {'status' : 200,'msj' : 'POST CALLED'}
-		data_to_predict = np.array([float(x) for x in request.form['data_to_predict'].split(',')]).reshape(1,2)
-		result = predict(request.form['userid'], request.form['modelid'], data_to_predict) 
-		return jsonify(result.prediction)
-	else:
-		respond = {'status' : 200,'msj' : 'GET CALLED'}
-		return jsonify(respond)
+    if request.method == 'POST':
+        respond = {'status': 200, 'msj': 'POST CALLED'}
+        img = json.loads(request.data)['data_to_predict']
+        result = predict(img)
+        return jsonify(result.prediction)
+    else:
+        respond = {'status': 200, 'msj': 'GET CALLED'}
+        return jsonify(respond)
 
-@blueprint.route('/learn_and_predict', methods=['GET','POST'])
+
+@blueprint.route('/learn_and_predict', methods=['GET', 'POST'])
 def learn_and_predict_it():
-	if request.method == 'POST':
-		respond = {'status' : 200,'msj' : 'POST CALLED'}
-		data_target = np.array([float(x) for x in request.form['data_target'].split(',')]).reshape(3,2)
-		data_to_predict = np.array([float(x) for x in request.form['data_to_predict'].split(',')]).reshape(1,2)
-		result = learn_and_predict(request.form['userid'], data_target, data_to_predict) 
-		return jsonify(result.prediction)
-	else:
-		respond = {'status' : 200,'msj' : 'GET CALLED'}
-		return jsonify(respond)
+    if request.method == 'POST':
+        respond = {'status': 200, 'msj': 'POST CALLED'}
+        data_target = np.array(
+            [float(x) for x in request.form['data_target'].split(',')]).reshape(3, 2)
+        data_to_predict = np.array(
+            [float(x) for x in request.form['data_to_predict'].split(',')]).reshape(1, 2)
+        result = learn_and_predict(
+            request.form['userid'], data_target, data_to_predict)
+        return jsonify(result.prediction)
+    else:
+        respond = {'status': 200, 'msj': 'GET CALLED'}
+        return jsonify(respond)
--- BlueprintArchitech/BijakSana/codeathonai/v1.py
+++ BlueprintArchitech/BijakSana/codeathonai/v1.py
@@ -1,56 +1,63 @@
-from sklearn import svm #contoh
-import setting 
+from sklearn import svm  # contoh
+import setting
 import pickle
 import os
 import random
 import datetime
+from fastai.vision import *
+from io import BytesIO
+import base64
+from binascii import a2b_base64
+import string
+ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+
+def model_predict(img_path):
+    """
+       model_predict will return the preprocessed image
+    """
+
+    img = open_image(img_path)
+    learn = load_learner('codeathonX/model/')
+    pred_class, pred_idx, outputs = learn.predict(img)
+    top3 = torch.topk(outputs, k=5)
+    return [ascii_uppercase[i] for i in top3[1]]
+
 
 class learn:
-	"""docstring"""
-	def __init__(self, userid, data_target):
-		X = data_target[:-1]
-		Y = data_target[-1]
-		self.model_fitted = svm.SVC(gamma=0.001).fit(X, Y) #contoh
-		# save model here
-		model_key = str(random.randint(100000000000,999999999999))
-		today = str(datetime.date.today())
-		pickle_meta = { 
-			'created_at':today,
-			'model_key':model_key }
-		model_id = str(hash(frozenset(pickle_meta.items())))
-		user_path = setting.model_folder+"/"+userid
-		path = user_path+"/"+model_id+".pkl"
-		try:
-		    os.makedirs(user_path)
-		except FileExistsError:
-		    # directory already exists 
-		    pass
-		pickle.dump(self.model_fitted, open(path, 'wb'))
-		# return
-		self.modelid = {
-			'result' 			: model_id,#list(self.model_fitted.predict(data_to_predict)),# return kan modelid
-		}
+    """docstring"""
+
+    def __init__(self, userid, data_target):
+        learn = load_learner('../../../model/')
+
 
 class predict:
-	"""docstring"""
-	def __init__(self, userid, modelid, data_to_predict):
-		# load model here
-		path = setting.model_folder+"/"+userid+"/"+modelid+".pkl"
-		print(path)
-		loaded_model = pickle.load(open(path, 'rb'))
-		# return
-		self.prediction = {
-			'result' 			: list(loaded_model.predict(data_to_predict)),
-		}
+    """docstring"""
+
+    def __init__(self, img):
+
+        imgstr = re.search(r'base64,(.*)', img).group(1)
+        output_fname = ''.join(random.choices(
+            string.ascii_uppercase + string.digits, k=6))
+        output = open(output_fname, 'wb')
+        output.write(base64.b64decode(imgstr))
+        output.close()
+        result = model_predict(output_fname)
+        # return
+        self.prediction = {
+            'result' 			: result,
+        }
+
 
 class learn_and_predict:
-	"""docstring"""
-	def __init__(self, userid, data_target, data_to_predict):
-		X = data_target[:-1]
-		Y = data_target[-1]
-		self.model_fitted = svm.SVC(gamma=0.001).fit(X, Y)
+    """docstring"""
 
-		# return
-		self.prediction = {
-			'result' 			: list(self.model_fitted.predict(data_to_predict)),
-		}
+    def __init__(self, userid, data_target, data_to_predict):
+        X = data_target[:-1]
+        Y = data_target[-1]
+        self.model_fitted = svm.SVC(gamma=0.001).fit(X, Y)
+
+        # return
+        self.prediction = {
+            'result' 			: list(self.model_fitted.predict(data_to_predict)),
+        }
